2fe3aed8bfa171244715d3ea819f5673fef5e6c8
Dmitry Eshman <bakko7821@gmail.com>
2026-01-28 21:26:10 +0300

    init new project
    -install nativewind
    -install expo (rn + ts)

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-red-300">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
