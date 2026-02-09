6a8315574bf727f43daa63710e35be77f8645366
Dmitry Eshman <bakko7821@gmail.com>
2026-02-09 19:45:26 +0300

    reset main

--- app/all-categories.tsx
+++
@@ -1,8 +0,0 @@
-import { useTheme } from "@/hooks/useTheme";
-import React from "react";
-import { View } from "react-native";
-
-export default function AllCategoriesScreen() {
-  const theme = useTheme();
-  return <View style={{ backgroundColor: theme.background }}></View>;
-}
--- components/UI/Plug.tsx
+++
@@ -1,8 +0,0 @@
-import { useTheme } from "@/hooks/useTheme";
-import React from "react";
-import { View } from "react-native";
-
-export default function Plug () {
-    const theme = useTheme()
-    return <View style={{backgroundColor: theme.secondary}} className="w-full h-[2px] rounded-xl"></View>
-}
