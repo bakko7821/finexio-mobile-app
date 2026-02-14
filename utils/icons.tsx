e8b0d3f3e1eced61d2401bea3018e0a237a5ba2f
Dmitry Eshman <bakko7821@gmail.com>
2026-02-15 00:57:59 +0300

    add 65 icons

--- utils/icons.tsx
+++ utils/icons.tsx
@@ -1,42 +1,107 @@
 // ENTERTAINMENTS
 import AmusementParkIcon from "@/assets/icons/entertainments/AmusementPark.svg";
+import BallonIcon from "@/assets/icons/entertainments/BalloonFill.svg";
 import SportIcon from "@/assets/icons/entertainments/BaselineSportsHandball.svg";
+import BilliardIcon from "@/assets/icons/entertainments/BilliardsFill.svg";
+import BoardGameIcon from "@/assets/icons/entertainments/BoardGames20Regular.svg";
 import BookIcon from "@/assets/icons/entertainments/BookOutline.svg";
+import BowlingIcon from "@/assets/icons/entertainments/BowlingStrike.svg";
+import MicrophoneIcon from "@/assets/icons/entertainments/BxsMicrophone.svg";
+import CameraIcon from "@/assets/icons/entertainments/CameraOutlined.svg";
+import PhotographyIcon from "@/assets/icons/entertainments/CameraWithFlash.svg";
+import ChessIcon from "@/assets/icons/entertainments/Chess.svg";
 import CinemaIcon from "@/assets/icons/entertainments/CinemaOld.svg";
+import ConfetiIcon from "@/assets/icons/entertainments/ConfettiBall.svg";
+import DiceIcon from "@/assets/icons/entertainments/Dice.svg";
+import DroneIcon from "@/assets/icons/entertainments/Drone.svg";
+import FireworksIcon from "@/assets/icons/entertainments/Fireworks.svg";
 import GameIcon from "@/assets/icons/entertainments/GameConsole.svg";
+import HeadphonesIcon from "@/assets/icons/entertainments/Headphones.svg";
+import KaraokeIcon from "@/assets/icons/entertainments/Karaoke.svg";
 import MuseumIcon from "@/assets/icons/entertainments/MuseumOne.svg";
 import MusicIcon from "@/assets/icons/entertainments/Music.svg";
 import PaletteIcon from "@/assets/icons/entertainments/Palette.svg";
+import PartyIcon from "@/assets/icons/entertainments/PartyingFace.svg";
+import CardsIcon from "@/assets/icons/entertainments/PlayingCardsRounded.svg";
+import PuzzleIcon from "@/assets/icons/entertainments/PuzzlePiece.svg";
+import StreamIcon from "@/assets/icons/entertainments/Streaming.svg";
 import TheaterIcon from "@/assets/icons/entertainments/Theater.svg";
 import TicketsIcon from "@/assets/icons/entertainments/Tickets.svg";
 import VideoIcon from "@/assets/icons/entertainments/VideoRecording.svg";
+import VrIcon from "@/assets/icons/entertainments/VrHeadset2.svg";
 
 // FOOD
+import AppleIcon from "@/assets/icons/food/Apple.svg";
+import BananaIcon from "@/assets/icons/food/Banana.svg";
 import BeerIcon from "@/assets/icons/food/BeerOutline.svg";
+import CakeIcon from "@/assets/icons/food/BirthdayCake.svg";
 import SaladIcon from "@/assets/icons/food/BowlSalad20Regular.svg";
 import BreadIcon from "@/assets/icons/food/Bread.svg";
 import BurgerIcon from "@/assets/icons/food/BurgerLine.svg";
+import BurritoIcon from "@/assets/icons/food/BurritoFastfood.svg";
+import ChampangeIcon from "@/assets/icons/food/Champagne.svg";
+import CheeseIcon from "@/assets/icons/food/CheeseWedge.svg";
+import ChocolateIcon from "@/assets/icons/food/ChocolateBar.svg";
+import CocktailIcon from "@/assets/icons/food/CocktailGlass.svg";
 import CoffeeIcon from "@/assets/icons/food/Coffee.svg";
+import CookieIcon from "@/assets/icons/food/Cookie.svg";
+import CupcakeIcon from "@/assets/icons/food/Cupcake.svg";
 import SodaIcon from "@/assets/icons/food/CupSoda.svg";
 import DonutIcon from "@/assets/icons/food/DonutBroken.svg";
 import SteakIcon from "@/assets/icons/food/FoodSteakCookGrillSteakBbqCookingNutritionBarbecueGrillingFood.svg";
 import FruitIcon from "@/assets/icons/food/FruitCherries.svg";
+import WhiskeyIcon from "@/assets/icons/food/GlassWhiskey.svg";
+import HoneyIcon from "@/assets/icons/food/Honey.svg";
+import HotdogIcon from "@/assets/icons/food/Hotdog.svg";
 import IceCreamIcon from "@/assets/icons/food/IcecreamTwo.svg";
+import MilkIcon from "@/assets/icons/food/Milk.svg";
+import TeaIcon from "@/assets/icons/food/MugTea.svg";
 import NoodlesIcon from "@/assets/icons/food/NoodleBowl.svg";
+import PancakeIcon from "@/assets/icons/food/Pancakes.svg";
 import PizzaIcon from "@/assets/icons/food/PizzaSlice.svg";
+import RamenIcon from "@/assets/icons/food/RamenDiningRounded.svg";
+import SandwichIcon from "@/assets/icons/food/Sandwich.svg";
+import SushiIcon from "@/assets/icons/food/Sushi.svg";
+import TacoIcon from "@/assets/icons/food/Taco.svg";
+import FishIcon from "@/assets/icons/food/TropicalFish.svg";
+import WaffleIcon from "@/assets/icons/food/Waffle.svg";
+import WineIcon from "@/assets/icons/food/WineGlass.svg";
 
 // TRANSPORT
+import TramIcon from "@/assets/icons/transport/AerialTramway.svg";
 import AirplaneIcon from "@/assets/icons/transport/AirplaneLine.svg";
+import HelmetIcon from "@/assets/icons/transport/AstronautHelmet.svg";
 import ScooterIcon from "@/assets/icons/transport/BaselineElectricScooter.svg";
 import BikeIcon from "@/assets/icons/transport/Bike.svg";
 import BoatIcon from "@/assets/icons/transport/BoatLine.svg";
+import BridgeIcon from "@/assets/icons/transport/BridgeWater.svg";
 import BusIcon from "@/assets/icons/transport/Bus.svg";
+import BatteryIcon from "@/assets/icons/transport/CarBattery.svg";
 import CarIcon from "@/assets/icons/transport/CarTwotone.svg";
+import ChargingStationIcon from "@/assets/icons/transport/ChargingStation.svg";
+import EngineIcon from "@/assets/icons/transport/Engine24Regular.svg";
+import FerryIcon from "@/assets/icons/transport/FerrySolid.svg";
 import GasIcon from "@/assets/icons/transport/GasStation.svg";
+import HotelIcon from "@/assets/icons/transport/Hotel.svg";
+import SubwayIcon from "@/assets/icons/transport/IosSubway.svg";
+import LuggageIcon from "@/assets/icons/transport/LuggageFill.svg";
+import MapIcon from "@/assets/icons/transport/MapSolid.svg";
+import MonorailIcon from "@/assets/icons/transport/Monorail.svg";
+import CarWashIcon from "@/assets/icons/transport/OutlineLocalCarWash.svg";
 import ParkingIcon from "@/assets/icons/transport/Parking.svg";
+import PassportIcon from "@/assets/icons/transport/Passport.svg";
+import RoadIcon from "@/assets/icons/transport/Road.svg";
+import RocketIcon from "@/assets/icons/transport/RocketTakeoff.svg";
+import SeatbeltIcon from "@/assets/icons/transport/SeatbeltLight.svg";
+import SpaceshipIcon from "@/assets/icons/transport/Spaceship.svg";
+import SpeedometerIcon from "@/assets/icons/transport/Speedometer.svg";
+import SuitcaseIcon from "@/assets/icons/transport/SuitcaseFill.svg";
 import TaxiIcon from "@/assets/icons/transport/Taxi.svg";
 import TireIcon from "@/assets/icons/transport/Tire.svg";
+import TowTruckIcon from "@/assets/icons/transport/TowTruck.svg";
+import TrafficLightsIcon from "@/assets/icons/transport/TrafficLight.svg";
 import TrainIcon from "@/assets/icons/transport/TrainProfile.svg";
+import YachtIcon from "@/assets/icons/transport/Yacht.svg";
 
 // SHOPPING
 import BootIcon from "@/assets/icons/shopping/Boot.svg";
@@ -112,6 +177,29 @@ export const iconsArray = [
       { id: 9, name: "steak", Icon: SteakIcon },
       { id: 10, name: "noodles", Icon: NoodlesIcon },
       { id: 11, name: "bread", Icon: BreadIcon },
+      { id: 65, name: "apple", Icon: AppleIcon },
+      { id: 66, name: "banana", Icon: BananaIcon },
+      { id: 67, name: "cheese", Icon: CheeseIcon },
+      { id: 68, name: "fish", Icon: FishIcon },
+      { id: 69, name: "sushi", Icon: SushiIcon },
+      { id: 70, name: "ramen", Icon: RamenIcon },
+      { id: 71, name: "hotdog", Icon: HotdogIcon },
+      { id: 72, name: "sandwich", Icon: SandwichIcon },
+      { id: 73, name: "taco", Icon: TacoIcon },
+      { id: 74, name: "burrito", Icon: BurritoIcon },
+      { id: 75, name: "pancake", Icon: PancakeIcon },
+      { id: 76, name: "waffle", Icon: WaffleIcon },
+      { id: 77, name: "cake", Icon: CakeIcon },
+      { id: 78, name: "cookie", Icon: CookieIcon },
+      { id: 79, name: "chocolate", Icon: ChocolateIcon },
+      { id: 80, name: "honey", Icon: HoneyIcon },
+      { id: 81, name: "milk", Icon: MilkIcon },
+      { id: 82, name: "tea", Icon: TeaIcon },
+      { id: 83, name: "wine", Icon: WineIcon },
+      { id: 84, name: "cocktail", Icon: CocktailIcon },
+      { id: 85, name: "whiskey", Icon: WhiskeyIcon },
+      { id: 86, name: "champagne", Icon: ChampangeIcon },
+      { id: 87, name: "cupcake", Icon: CupcakeIcon },
     ],
   },
   {
@@ -129,6 +217,29 @@ export const iconsArray = [
       { id: 20, name: "taxi", Icon: TaxiIcon },
       { id: 21, name: "tire", Icon: TireIcon },
       { id: 22, name: "train", Icon: TrainIcon },
+      { id: 88, name: "subway", Icon: SubwayIcon },
+      { id: 89, name: "tram", Icon: TramIcon },
+      { id: 90, name: "monorail", Icon: MonorailIcon },
+      { id: 91, name: "ferry", Icon: FerryIcon },
+      { id: 92, name: "yacht", Icon: YachtIcon },
+      { id: 93, name: "rocket", Icon: RocketIcon },
+      { id: 94, name: "spaceship", Icon: SpaceshipIcon },
+      { id: 95, name: "helmet", Icon: HelmetIcon },
+      { id: 96, name: "seatbelt", Icon: SeatbeltIcon },
+      { id: 97, name: "speedometer", Icon: SpeedometerIcon },
+      { id: 98, name: "engine", Icon: EngineIcon },
+      { id: 99, name: "battery", Icon: BatteryIcon },
+      { id: 100, name: "charging-station", Icon: ChargingStationIcon },
+      { id: 101, name: "car-wash", Icon: CarWashIcon },
+      { id: 102, name: "tow-truck", Icon: TowTruckIcon },
+      { id: 103, name: "map", Icon: MapIcon },
+      { id: 104, name: "road", Icon: RoadIcon },
+      { id: 105, name: "traffic-light", Icon: TrafficLightsIcon },
+      { id: 106, name: "bridge", Icon: BridgeIcon },
+      { id: 107, name: "luggage", Icon: LuggageIcon },
+      { id: 108, name: "suitcase", Icon: SuitcaseIcon },
+      { id: 109, name: "passport", Icon: PassportIcon },
+      { id: 110, name: "hotel", Icon: HotelIcon },
     ],
   },
   {
@@ -146,6 +257,25 @@ export const iconsArray = [
       { id: 31, name: "theater", Icon: TheaterIcon },
       { id: 32, name: "tickets", Icon: TicketsIcon },
       { id: 33, name: "video", Icon: VideoIcon },
+      { id: 111, name: "headphones", Icon: HeadphonesIcon },
+      { id: 112, name: "microphone", Icon: MicrophoneIcon },
+      { id: 113, name: "camera", Icon: CameraIcon },
+      { id: 114, name: "photography", Icon: PhotographyIcon },
+      { id: 115, name: "drone", Icon: DroneIcon },
+      { id: 116, name: "stream", Icon: StreamIcon },
+      { id: 117, name: "vr-headset", Icon: VrIcon },
+      { id: 118, name: "board-game", Icon: BoardGameIcon },
+      { id: 119, name: "puzzle", Icon: PuzzleIcon },
+      { id: 120, name: "chess", Icon: ChessIcon },
+      { id: 121, name: "cards", Icon: CardsIcon },
+      { id: 122, name: "dice", Icon: DiceIcon },
+      { id: 123, name: "bowling", Icon: BowlingIcon },
+      { id: 124, name: "billiard", Icon: BilliardIcon },
+      { id: 125, name: "karaoke", Icon: KaraokeIcon },
+      { id: 126, name: "fireworks", Icon: FireworksIcon },
+      { id: 127, name: "party", Icon: PartyIcon },
+      { id: 128, name: "balloon", Icon: BallonIcon },
+      { id: 129, name: "confetti", Icon: ConfetiIcon },
     ],
   },
   {
