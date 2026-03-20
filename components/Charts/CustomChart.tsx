d91b2b4acb9b51b92ff171b393eb3e222fba8c3d
Dmitry Eshman <bakko7821@gmail.com>
2026-03-21 01:54:44 +0300

    style: try create customChart

--- components/Charts/CustomChart.tsx
+++ components/Charts/CustomChart.tsx
@@ -1,6 +1,9 @@
 import { useTheme } from "@/hooks/useTheme";
 import { PieItem } from "@/utils/types/chart";
-import { TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+
+import { withOpacity } from "@/utils/colors";
+import { RenderIcon } from "../UI/RenderIcon";
 
 interface CustomChartProps {
   data: PieItem[];
@@ -13,9 +16,30 @@ export default function CustomChart({ data, changeType }: CustomChartProps) {
   return (
     <TouchableOpacity
       activeOpacity={0.8}
-      style={{ backgroundColor: theme.card }}
-      className="flex-1 w-full relative"
       onPress={changeType}
-    ></TouchableOpacity>
+      className="w-full flex-1 flex-wrap flex-row items-start justify-between gap-2"
+    >
+      {data.map((item) => (
+        <View
+          key={item.value}
+          className="items-center justify-center flex-col gap-1"
+        >
+          <View
+            style={{ backgroundColor: withOpacity(item.color, 0.4) }}
+            className="rounded-full items-center justify-center p-2"
+          >
+            <RenderIcon
+              width={32}
+              height={32}
+              name={item.icon}
+              color={item.color}
+            />
+          </View>
+          <Text style={{ color: item.color }} className="text-xs font-medium">
+            {item.value} ₽
+          </Text>
+        </View>
+      ))}
+    </TouchableOpacity>
   );
 }
