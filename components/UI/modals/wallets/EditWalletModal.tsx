b40a118b83affa149c292b9b8b8fc78374044bf8
Dmitry Eshman <bakko7821@gmail.com>
2026-03-31 21:19:58 +0300

    style: done editWalletModal

--- components/UI/modals/wallets/EditWalletModal.tsx
+++ components/UI/modals/wallets/EditWalletModal.tsx
@@ -1,9 +1,20 @@
 import CrossIcon from "@/assets/ui/CrossFilled.svg";
+import BackArrowIcon from "@/assets/ui/arrow-prev-small-svgrepo-com.svg";
+import PickColorComponent from "@/components/Categories/PickColorComponent";
+import PickIconComponent from "@/components/Categories/PickIconComponent";
 import { useTheme } from "@/hooks/useTheme";
 import { getContrastColor } from "@/utils/colors";
 import { Wallet } from "@/utils/types/wallet";
-import { Text, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Modal from "react-native-modal";
+import { RenderIcon } from "../../RenderIcon";
 
 interface EditWalletModalProps {
   visible: boolean;
@@ -19,6 +30,34 @@ export default function EditWalletModal({
   onRefresh,
 }: EditWalletModalProps) {
   const theme = useTheme();
+  const [walletNameValue, setWalletNameValue] = useState("");
+  const [walletValue, setWalletValue] = useState("");
+  const [selectedIcon, setSelectedIcon] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
+
+  const [isVisiblePickColor, setIsVisiblePickColor] = useState(false);
+  const [isVisiblePickIcon, setIsVisiblePickIcon] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [headerTitle, setHeaderTitle] = useState("");
+
+  useEffect(() => {
+    if (!wallet) return;
+
+    setHeaderTitle(`Изменение счёта: ${wallet.name}`);
+    setWalletNameValue(wallet.name);
+    setSelectedIcon(wallet.icon);
+    setSelectedColor(wallet.color);
+    setWalletValue(`${wallet.value}`);
+  }, [wallet]);
+
+  const getBackHandle = () => {
+    if (!wallet) return;
+
+    setHeaderTitle(`Изменение счёта: ${wallet.name}`);
+    setIsEdit(false);
+    setIsVisiblePickColor(false);
+    setIsVisiblePickIcon(false);
+  };
 
   if (wallet)
     return (
@@ -39,32 +78,156 @@ export default function EditWalletModal({
           className="overflow-hidden rounded-t-3xl flex-col"
         >
           <View className="p-4 w-full items-center justify-between flex-row">
-            <Text
-              style={{ color: theme.text }}
-              className="text-base font-medium"
-            >
-              Изменение счёта:{" "}
-              <Text style={{ color: wallet.color }}>{wallet.name}</Text>
-            </Text>
+            <View className="flex-row gap-2 items-center justify-center">
+              {isEdit && (
+                <TouchableOpacity onPress={() => getBackHandle()}>
+                  <BackArrowIcon width={24} height={24} color={theme.text} />
+                </TouchableOpacity>
+              )}
+              <Text
+                style={{ color: theme.text }}
+                className="text-base font-medium"
+              >
+                {headerTitle}
+              </Text>
+            </View>
             <TouchableOpacity onPress={onClose}>
               <CrossIcon width={24} height={24} color={theme.text} />
             </TouchableOpacity>
           </View>
-          <View></View>
-          <View className="p-4 w-full items-center justify-center">
-            <TouchableOpacity
-              style={{ backgroundColor: theme.primary }}
-              className="p-3 rounded-full w-full items-center justify-center"
-            >
-              <Text
-                style={{ color: getContrastColor(theme.primary) }}
-                className="text-base font-medium"
-              >
-                Сохранить
-              </Text>
-            </TouchableOpacity>
-          </View>
+          {!isEdit && (
+            <>
+              <View className="w-full flex-col gap-2">
+                <View className="px-4 w-full flex-row items-center justify-between ">
+                  <Text
+                    style={{ color: theme.secondary }}
+                    className="text-base font-medium"
+                  >
+                    Название:
+                  </Text>
+                  <TextInput
+                    style={{
+                      color: theme.text,
+                      borderColor: theme.text,
+                    }}
+                    placeholder='Например: "Наличные"'
+                    placeholderTextColor={theme.secondary}
+                    {...Platform.select({
+                      android: { includeFontPadding: false },
+                    })}
+                    value={walletNameValue}
+                    className="p-1 px-2 border-b-[2px] border-solid"
+                    onChangeText={setWalletNameValue}
+                  />
+                </View>
+                <View className="px-4 w-full flex-row items-center justify-between">
+                  <Text
+                    style={{ color: theme.secondary }}
+                    className="text-base font-medium"
+                  >
+                    Значение:
+                  </Text>
+                  <TextInput
+                    keyboardType="number-pad"
+                    style={{
+                      color: theme.text,
+                      borderColor: theme.text,
+                    }}
+                    placeholder='Например: "Наличные"'
+                    placeholderTextColor={theme.secondary}
+                    {...Platform.select({
+                      android: { includeFontPadding: false },
+                    })}
+                    value={walletValue + " ₽"}
+                    onChangeText={(text) => {
+                      const cleaned = text.replace(/ ₽/g, "");
+                      setWalletValue(cleaned);
+                    }}
+                    className="p-1 px-2 border-b-[2px] border-solid"
+                  />
+                </View>
+                <View className="px-4 w-full flex-row items-center justify-between">
+                  <Text
+                    style={{ color: theme.secondary }}
+                    className="text-base font-medium"
+                  >
+                    Иконка:
+                  </Text>
+                  <TouchableOpacity
+                    style={{ backgroundColor: theme.card }}
+                    className="p-2 rounded-xl"
+                    onPress={() => {
+                      setIsEdit(true);
+                      setIsVisiblePickIcon(true);
+                    }}
+                  >
+                    <RenderIcon
+                      name={selectedIcon}
+                      width={24}
+                      height={24}
+                      color={theme.text}
+                    />
+                  </TouchableOpacity>
+                </View>
+                <View className="px-4 w-full flex-row items-center justify-between">
+                  <Text
+                    style={{ color: theme.secondary }}
+                    className="text-base font-medium"
+                  >
+                    Цвет:
+                  </Text>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: selectedColor,
+                    }}
+                    className="p-2 rounded-xl"
+                    onPress={() => {
+                      setIsEdit(true);
+                      setIsVisiblePickColor(true);
+                    }}
+                  >
+                    <View className="w-[24px] h-[24px]"></View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View className="p-4 w-full items-center justify-center">
+                <TouchableOpacity
+                  style={{ backgroundColor: theme.primary }}
+                  className="p-3 rounded-full w-full items-center justify-center"
+                >
+                  <Text
+                    style={{ color: getContrastColor(theme.primary) }}
+                    className="text-base font-medium"
+                  >
+                    Сохранить
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </>
+          )}
         </View>
+        {isVisiblePickIcon && (
+          <View
+            style={{ backgroundColor: theme.header }}
+            className="h-[60%] px-4"
+          >
+            <PickIconComponent
+              selectedIcon={selectedIcon}
+              onSelect={setSelectedIcon}
+            />
+          </View>
+        )}
+        {isVisiblePickColor && (
+          <View
+            style={{ backgroundColor: theme.header }}
+            className="h-[60%] px-4"
+          >
+            <PickColorComponent
+              selectedColor={selectedColor}
+              onSelect={setSelectedColor}
+            />
+          </View>
+        )}
       </Modal>
     );
 }
